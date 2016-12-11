e9353044b024f39d30c1a9a90490c14424fa2b23
Brian Coe <bwc126@gmail.com>
2016-12-11 01:38:16 -0500

    feat: add bootstrap-datepicker to project

--- config/assets/default.js
+++ config/assets/default.js
@@ -18,11 +18,13 @@ module.exports = {
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         'https://use.fontawesome.com/9cc35c3faa.js',
+        'public/custom_lib/bootstrap-datepicker-1.6.4-dist/js/bootstrap-datepicker.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
     css: [
-      'modules/*/client/css/*.css'
+      'modules/*/client/css/*.css',
+      'public/custom_lib/bootstrap-datepicker-1.6.4-dist/css/bootstrap-datepicker3.min.css'
     ],
     less: [
       'modules/*/client/less/*.less'
