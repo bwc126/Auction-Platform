fc1c2814e27c7582be40e076f4e4a07d0753a2e6
Brian Coe <bwc126@gmail.com>
2017-01-13 22:51:21 -0500

    feat: Add LT total to site header

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,7 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-  function ($scope, $state, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$http', '$scope', '$state', 'Authentication', 'Menus',
+  function ($http, $scope, $state, Authentication, Menus) {
+    var updatePeriod = 3000;
+    var auctions;
+    var tableTotal;
+    $scope.tableTotal = 0;
     // Expose view variables
     $scope.$state = $state;
     $scope.authentication = Authentication;
@@ -22,5 +26,30 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.$on('$stateChangeSuccess', function () {
       $scope.isCollapsed = false;
     });
+
+    $http.get('/api/auctions').then(function(response) {
+      auctions = response.data;
+      // Get the total of the LT every few seconds
+      var bidUpdate = window.setInterval(function() {
+        tableTotal = 0;
+        auctions.forEach(function(auction) {
+          getCurrent(auction);
+        });
+
+      }, updatePeriod);
+      // Update the displayed LT value
+
+    });
+    function getCurrent(auction) {
+      $http.get('/api/bids/'+auction._id+'/leading').then(function(response) {
+        auction.amount = response.data ? response.data.amount.toFixed(2) : 1.00;
+        auction.leader = response.data ? response.data.user.displayName : '';
+        auction.currentUser = $scope.userName;
+        tableTotal += Number(auction.amount);
+        $scope.tableTotal = (tableTotal*0.8).toFixed(2);
+      });
+
+    }
+
   }
 ]);
