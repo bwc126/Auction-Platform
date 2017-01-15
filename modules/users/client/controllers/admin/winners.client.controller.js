45e54c58f37dc8f58f9de970cbe6ba19e48f5a58
Brian Coe <bwc126@gmail.com>
2017-01-14 21:15:16 -0500

    feat: Scaffold 'winners' controller

---
+++ modules/users/client/controllers/admin/winners.client.controller.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('users')
+    .controller('WinnersController', WinnersController);
+
+  WinnersController.$inject = ['$http', '$scope', '$state', '$window', 'Authentication'];
+
+  function WinnersController($http, $scope, $state, $window, Authentication) {
+    var vm = this;
+    // Use functions to get LT and auction winners
+    function getLTWinners() {
+      // Use the entries api to get a batch of LT Winners
+    }
+    function getAuctionWinner() {
+      // For each auction, get the winner's shipping and email info, and the item they won
+    }
+  }
+
+})
